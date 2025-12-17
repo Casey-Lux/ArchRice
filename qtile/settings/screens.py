bb1ead83d8a78d15810b9d0f85a5ec42cd8327fe
Casey-Lux <casey.lux.000@gmail.com>
2025-12-17 13:22:54 -0500

    Adjusting bar margin

--- qtile/settings/screens.py
+++ qtile/settings/screens.py
@@ -5,7 +5,7 @@ from libqtile import bar
 from settings.widgets import primary_widgets
 
 def status_bar(widgets):
-    return bar.Bar(widgets, 28, margin=[4, 6, 4, 6], opacity=0.95)
+    return bar.Bar(widgets, 32, margin=[6, 6, 4, 6], opacity=0.95)
 
 screens = [
     Screen(top=status_bar(primary_widgets))
