c8cf050361f2b9cefff2d82eea9c6b778d0d8b37
Casey-Lux <casey.lux.000@gmail.com>
2025-12-18 11:32:12 -0500

    Adding picom to autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -12,5 +12,5 @@ xrandr --output Virtual-1 --mode 1366x768 & # <- Adjust this for your screen
 # Wallpapers
 /home/user0/wallpapers/wallpaper.py &
 
-# picom: --no-vsync
-# picom -f &
+# picom
+picom --config ~/.config/picom/picom.conf &
